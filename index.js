c914b8419d2050978bd0f08f63530fdd4c9fb977
Aziz-Prithibee13 <elmataddelmar55@gmail.com>
2023-07-10 00:39:56 +0600

    delation

--- index.js
+++ index.js
@@ -33,14 +33,6 @@ async function run() {
             modelUrl: "https://teachablemachine.withgoogle.com/models/V2vfM0Dv-/",
         });
 
-        const storage = multer.diskStorage({
-            destination(req, file, callback) {
-                callback(null, './images');
-            },
-            filename(req, file, callback) {
-                callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-            },
-        });
 
 
         const moviesCollection = client.db("Movie-Mania").collection("Moies");
